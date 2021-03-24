b8bd39829622bb92665300a383efe47d56c70b06
quochuytran98 <ltht1999@gmail.com>
2021-03-24 14:48:39 +0700

    commit1

---
+++ login.php
@@ -0,0 +1,28 @@
+<!-- Tran Quoc Huy 1711060583
+Truong Dang Khoa
+VO Hong Tu -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+
+    
+<form method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1" name="lbUserName">NAME</label>
+    <input type="text" class="form-control"placeholder="Enter username">
+  </div>
+  
+  
+  
+</form>
+</body>
+</html>
